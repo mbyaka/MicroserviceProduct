ac9dd3f7153478bdd14bfd471ee180e9550e00c9
halukalp <haluk.allp@gmail.com>
2022-06-06 19:34:19 +0300

    initial commit

--- src/main/java/com/infinidium/postgresql/admin/products/SpringBootMicroserviceProductApplication.java
+++ src/main/java/com/infinidium/postgresql/admin/products/SpringBootMicroserviceProductApplication.java
@@ -3,8 +3,10 @@ package com.infinidium.postgresql.admin.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroserviceProductApplication {
 
 	public static void main(String[] args) {
